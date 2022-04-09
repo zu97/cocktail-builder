710f989f6c6ea24fc20b6ecdae687f72cebdccde
admin <admin@admin.com>
2022-04-09 10:53:53 +0600

    Create API middleware

---
+++ api/middleware/permit.js
@@ -0,0 +1,15 @@
+const  permit = (...roles) => {
+    return (req, res, next) => {
+        if (!req.user) {
+            return res.status(400).send({error: 'Unauthenticated'});
+        }
+
+        if (!roles.includes(req.user.role)) {
+            return res.status(400).send({error: 'Unauthorized'});
+        }
+
+        next();
+    };
+};
+
+module.exports = permit;
