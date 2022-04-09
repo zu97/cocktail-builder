3f4beafe5fbde531ee014b642052a864693b26d2
admin <admin@admin.com>
2022-04-09 10:44:05 +0600

    Setting up a store for the frontend

--- frontend/src/app/app-store.module.ts
+++ frontend/src/app/app-store.module.ts
@@ -1,11 +1,35 @@
 import { NgModule } from '@angular/core';
-import { StoreModule } from '@ngrx/store';
+import { ActionReducer, MetaReducer, StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { usersReducer } from './store/users.reducer';
+import { cocktailsReducer } from './store/cocktails.reducer';
+import { UsersEffects } from './store/users.effects';
+import { CocktailsEffects } from './store/cocktails.effects';
+import { localStorageSync } from 'ngrx-store-localstorage';
+
+const localStorageSyncReducer = (reducer: ActionReducer<any>) => {
+  return localStorageSync({
+    keys: [{users: ['user']}],
+    rehydrate: true,
+  })(reducer);
+};
+
+const metaReducers: MetaReducer[] = [localStorageSyncReducer];
+
+const reducers = {
+  users: usersReducer,
+  cocktails: cocktailsReducer,
+};
+
+const effects = [
+  UsersEffects,
+  CocktailsEffects,
+];
 
 @NgModule({
   imports: [
-    StoreModule.forRoot({}, {}),
-    EffectsModule.forRoot([]),
+    StoreModule.forRoot(reducers, {metaReducers}),
+    EffectsModule.forRoot(effects),
   ],
   exports: [StoreModule, EffectsModule]
 })
