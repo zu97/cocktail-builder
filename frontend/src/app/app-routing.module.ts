0c1c40916b02ed1150b884dd7c6887cdaf0bc481
admin <admin@admin.com>
2022-04-09 10:49:05 +0600

    Setting up routing for the frontend

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponents } from './page-not-found.components';
+import { EditCocktailComponent } from './pages/edit-cocktail/edit-cocktail.component';
+import { CocktailsComponent } from './pages/cocktails/cocktails.component';
+import { CocktailDetailsComponent } from './pages/cocktail-details/cocktail-details.component';
+import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
+  {path: '', component: CocktailsComponent},
+  {path: 'cocktails', children: [
+    {path: '', component: CocktailsComponent},
+    {path: 'my', component: CocktailsComponent, canActivate: [AuthGuardService]},
+    {path: 'create', component: EditCocktailComponent, canActivate: [AuthGuardService]},
+    {path: ':id', component: CocktailDetailsComponent},
+  ]},
   {path: '**', component: PageNotFoundComponents},
 ];
 
